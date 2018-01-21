25a4d961d823eeed41a9a33502103c3d0735ee8b
Yacine Hmito <yacine.hmito@gmail.com>
2018-01-21 19:23:42 +0100

    Polyfill String.prototype.padEnd

--- src/format/index.js
+++ src/format/index.js
@@ -1,3 +1,10 @@
+if (!String.prototype.padEnd) {
+  const padEndPolyfill = require("pad-end");
+  String.prototype.padEnd = function padEnd(...args) {
+    return padEndPolyfill(this, ...args);
+  };
+}
+
 module.exports = {
   compact: require("./compact"),
   json: require("./json"),
